771b05bb98c625b9dc8a83954fba2e26606e2d2b
Alex Elhage <ahage16@gmail.com>
2012-05-24 11:30:14 -0400

    Added alignment options for VerticalLayout

--- src/slickMenus/layouts/VerticalLayout.java
+++ src/slickMenus/layouts/VerticalLayout.java
@@ -9,6 +9,50 @@ import slickMenus.elements.Element;
 public class VerticalLayout implements LayoutManager {
 
 	/**
+	 * Value which indicates left alignment
+	 */
+	public static final int LEFT = 0;
+	
+	/**
+	 * Value which indicates center alignment
+	 */
+	public static final int CENTER = 1;
+	
+	/**
+	 * Value which indicates right alignment
+	 */
+	public static final int RIGHT = 2;
+	
+	private int _alignment;
+	
+	/**
+	 * VerticalLayout constructor with a default alignment of CENTER
+	 */
+	public VerticalLayout() {
+		this(1);
+	}
+	
+	/**
+	 * VerticalLayout constructor with a specified alignment value
+	 * @param alignment Alignment to use. A value of 0 is left aligned, a 
+	 * value of 1 is centered, a value of 2 is right aligned. If the given 
+	 * alignment is not one of these values, this layout manager will not 
+	 * adjust the position of any elements.
+	 */
+	public VerticalLayout(int alignment) {
+		_alignment = alignment;
+	}
+	
+	/**
+	 * Set the new alignment for this VerticalLayout
+	 * @param alignment Alignment to use. A value of 0 is left aligned, a 
+	 * value of 1 is centered, a value of 2 is right aligned.
+	 */
+	public void setAlignment(int alignment) {
+		_alignment = alignment;
+	}
+	
+	/**
 	 * Update the position of all elements passed to this manager.
 	 * @param list List of elements
 	 * @param x X coordinate of top left corner
@@ -19,15 +63,42 @@ public class VerticalLayout implements LayoutManager {
 	public float[] updateLayout(LinkedList<Element> list, float x, float y) {
 		float width = 0f;
 		float height = 0f;
-		for (Element e : list) {
-			e.setPosY(y + e.getPadY());
-			height += e.getHeight() + 2 * e.getPadY();
-			y += e.getHeight() + 2 * e.getPadY();
-			float tempWidth = e.getWidth() + 2 * e.getPadX();
-			if (tempWidth > width) width = tempWidth;
-		}
-		for (Element e : list) {
-			e.setPosX(x + (width - e.getWidth()) / 2);
+		switch(_alignment) {
+		case LEFT: 
+			for (Element e: list) {
+				e.setPosY(y + e.getPadY());
+				height += e.getHeight() + 2 * e.getPadY();
+				y += e.getHeight() + 2 * e.getPadY();
+				e.setPosX(x + e.getPadX());
+				float tempWidth = e.getWidth() + 2 * e.getPadX();
+				if (tempWidth > width) width = tempWidth;
+			}
+		break;
+		case CENTER: 
+			for (Element e : list) {
+				e.setPosY(y + e.getPadY());
+				height += e.getHeight() + 2 * e.getPadY();
+				y += e.getHeight() + 2 * e.getPadY();
+				float tempWidth = e.getWidth() + 2 * e.getPadX();
+				if (tempWidth > width) width = tempWidth;
+			}
+			for (Element e : list) {
+				e.setPosX(x + (width - e.getWidth()) / 2);
+			}
+			break;
+		case RIGHT: 
+			for (Element e : list) {
+				e.setPosY(y + e.getPadY());
+				height += e.getHeight() + 2 * e.getPadY();
+				y += e.getHeight() + 2 * e.getPadY();
+				float tempWidth = e.getWidth() + 2 * e.getPadX();
+				if (tempWidth > width) width = tempWidth;
+			}
+			for (Element e : list) {
+				e.setPosX(x + width - (e.getWidth() + e.getPadX()));
+			}
+			break;
+		default: break;
 		}
 		return new float[] {width, height};
 	}
