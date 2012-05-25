cedcde7f3bbdaf03d6c321f70c20cfe67510646f
Alex Elhage <ahage16@gmail.com>
2012-05-25 14:11:19 -0400

    Finished implementing Toggle and ToggleListener

--- src/slickMenus/elements/Toggle.java
+++ src/slickMenus/elements/Toggle.java
@@ -2,6 +2,14 @@ package slickMenus.elements;
 
 import org.newdawn.slick.Image;
 
+import slickMenus.listeners.ActionListener;
+
+/**
+ * A toggle switch that can have a true or false state. Creating 
+ * a toggle automatically creates an ActionListener that changes the 
+ * state of the toggle when it is pressed. In order to change a setting 
+ * with a Toggle, you should add a ToggleListener.
+ */
 public class Toggle extends Element {
 	
 	/**
@@ -19,6 +27,12 @@ public class Toggle extends Element {
 	 */
 	private boolean _state;
 
+	/**
+	 * Constructs a Toggle
+	 * @param trueImage Image to draw when the state is true
+	 * @param falseImage Image to draw when the state is false
+	 * @param initState the initial state of the toggle
+	 */
 	public Toggle(Image trueImage, Image falseImage, boolean initState) {
 		_trueImage = trueImage;
 		_falseImage = falseImage;
@@ -31,13 +45,61 @@ public class Toggle extends Element {
 			_width = _falseImage.getWidth();
 			_height = _falseImage.getHeight();
 		}
+		
+		addActionListener(new ToggleListener(this));
 	}
 	
+	/**
+	 * Draws the backgroundImage, and the draws the correct image based on
+	 * the state of the toggle.
+	 */
 	@Override
 	public void draw(float frameX, float frameY) {
 		super.draw(frameX, frameY);
 		if (_state) _trueImage.draw(frameX + _xPos, frameY +_yPos);
 		else _falseImage.draw(frameX + _xPos, frameY + _yPos);
+	}
+	
+	/**
+	 * Negates the state of this toggle
+	 */
+	private void toggle() {
+		_state = !_state;
+	}
+	
+	/**
+	 * Returns the state of this toggle
+	 * @return The state of this toggle
+	 */
+	public boolean getState() {
+		return _state;
+	}
+	
+	private class ToggleListener implements ActionListener {
+
+		private Toggle _toggle;
+		
+		public ToggleListener(Toggle t) {
+			_toggle = t;
+		}
+		
+		@Override
+		public void mousePressed(int button, int x, int y, Element e) {
+		}
+
+		@Override
+		public void mouseReleased(int button, int x, int y, Element e) {
+			if (e.inRect(x, y)) _toggle.toggle();
+		}
+
+		@Override
+		public void mouseMoved(int oldX, int oldY, int newX, int newY, Element e) {
+		}
+
+		@Override
+		public void update(int delta, Element e) {
+		}
+		
 	}
 
 }
--- src/slickMenus/listeners/ToggleListener.java
+++ src/slickMenus/listeners/ToggleListener.java
@@ -1,23 +1,28 @@
 package slickMenus.listeners;
 
-import slickMenus.elements.Element;
+import slickMenus.elements.Toggle;
 
-public class ToggleListener implements ActionListener {
+/**
+ * An ActionListener intended to only by used by a Toggle. This should be 
+ * implemented by reading the state of the specified toggle using the getState
+ * method and then using that to carry out the desired action. The state is 
+ * automatically toggled by the built in ActionListener of a Toggle, so that 
+ * should not be done with this class.
+ */
+public abstract class ToggleListener implements ActionListener {
 
-	@Override
-	public void mousePressed(int button, int x, int y, Element e) {
-	}
-
-	@Override
-	public void mouseReleased(int button, int x, int y, Element e) {
-	}
-
-	@Override
-	public void mouseMoved(int oldX, int oldY, int newX, int newY, Element e) {
-	}
-
-	@Override
-	public void update(int delta, Element e) {
+	/**
+	 * Toggle that this listener will read the state from
+	 */
+	private Toggle _toggle;
+	
+	/**
+	 * Constructs a ToggleListener with a specified toggle to read the 
+	 * state from
+	 * @param t The Toggle this listener will read the state from.
+	 */
+	public ToggleListener(Toggle t) {
+		_toggle = t;
 	}
 
 }
